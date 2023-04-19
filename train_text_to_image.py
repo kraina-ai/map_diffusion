de48dfe99a781ee4f81730ca7b2f3820528bc31e
mprzymus <pzrymus@gmail.com>
2023-04-19 17:36:44 +0200

    feat: unet taken from cofig.py

--- train_text_to_image.py
+++ train_text_to_image.py
@@ -49,9 +49,8 @@ from diffusers.training_utils import EMAModel
 from diffusers.utils import check_min_version, deprecate, is_wandb_available
 from diffusers.utils.import_utils import is_xformers_available
 
-from config import DATA_DIR, N_COLUMNS
+from config import DATA_DIR, N_COLUMNS, BASE_MODEL_NAME, get_unet
 from osm_dataset import TextToImageDataset
-from config import BASE_MODEL_NAME
 
 if is_wandb_available():
     import wandb
@@ -453,6 +452,14 @@ def parse_args():
             " more information see https://huggingface.co/docs/accelerate/v0.17.0/en/package_reference/accelerator#accelerate.Accelerator"
         ),
     )
+    parser.add_argument(
+        "--custom_unet",
+        type=bool,
+        default=False,
+        help=(
+            "To get unet with function from config"
+        ),
+    )
 
     args = parser.parse_args()
     env_local_rank = int(os.environ.get("LOCAL_RANK", -1))
@@ -545,11 +552,15 @@ def main():
     vae = AutoencoderKL.from_pretrained(
         args.pretrained_model_name_or_path, subfolder="vae", revision=args.revision
     )
-    unet = UNet2DConditionModel.from_pretrained(
-        args.pretrained_model_name_or_path,
-        subfolder="unet",
-        revision=args.non_ema_revision,
-    )
+    if args.custom_unet:
+        print("Custom unet model")
+        unet = get_unet()
+    else:
+        unet = UNet2DConditionModel.from_pretrained(
+            args.pretrained_model_name_or_path,
+            subfolder="unet",
+            revision=args.non_ema_revision,
+        )
 
     # Freeze vae and text_encoder
     vae.requires_grad_(False)
@@ -690,93 +701,17 @@ def main():
     # In distributed training, the load_dataset function guarantees that only one local process can concurrently
     # download the dataset.
     if args.dataset_name is not None:
-        # Downloading and loading a dataset from the hub.
         dataset = TextToImageDataset(
             path=args.dataset_name, n_columns=N_COLUMNS
         ).to_huggingface_dataset()
-        # load_dataset(
-        #     args.dataset_name,
-        #     args.dataset_config_name,
-        #     cache_dir=args.cache_dir,
-        # )
     else:
         raise Exception("No dataset")
-        # See more about loading custom images at
-        # https://huggingface.co/docs/datasets/v2.4.0/en/image_load#imagefolder
 
     # Preprocessing the datasets.
     # We need to tokenize inputs and targets.
     column_names = dataset["train"].column_names
     print(column_names)
 
-    # 6. Get the column names for input/target.
-    dataset_columns = DATASET_NAME_MAPPING.get(args.dataset_name, None)
-    # if args.image_column is None:
-    #     image_column = (
-    #         dataset_columns[0] if dataset_columns is not None else column_names[0]
-    #     )
-    # else:
-    #     image_column = args.image_column
-    #     if image_column not in column_names:
-    #         raise ValueError(
-    #             f"--image_column' value '{args.image_column}' needs to be one of: {', '.join(column_names)}"
-    #         )
-    # if args.caption_column is None:
-    #     caption_column = (
-    #         dataset_columns[1] if dataset_columns is not None else column_names[1]
-    #     )
-    # else:
-    #     caption_column = args.caption_column
-    #     if caption_column not in column_names:
-    #         raise ValueError(
-    #             f"--caption_column' value '{args.caption_column}' needs to be one of: {', '.join(column_names)}"
-    #         )
-
-    # Preprocessing the datasets.
-    # We need to tokenize input captions and transform the images.
-    # def tokenize_captions(examples, is_train=True):
-    #     captions = []
-    #     for caption in examples[caption_column]:
-    #         if isinstance(caption, str):
-    #             captions.append(caption)
-    #         elif isinstance(caption, (list, np.ndarray)):
-    #             # take a random caption if there are multiple
-    #             captions.append(random.choice(caption) if is_train else caption[0])
-    #         else:
-    #             raise ValueError(
-    #                 f"Caption column `{caption_column}` should contain either strings or lists of strings."
-    #             )
-    #     inputs = tokenizer(
-    #         captions,
-    #         max_length=tokenizer.model_max_length,
-    #         padding="max_length",
-    #         truncation=True,
-    #         return_tensors="pt",
-    #     )
-    #     return inputs.input_ids
-
-    # Preprocessing the datasets.
-    # train_transforms = transforms.Compose(
-    #     [
-    #         transforms.Resize(
-    #             args.resolution, interpolation=transforms.InterpolationMode.BILINEAR
-    #         ),
-    #         transforms.CenterCrop(args.resolution)
-    #         if args.center_crop
-    #         else transforms.RandomCrop(args.resolution),
-    #         transforms.RandomHorizontalFlip()
-    #         if args.random_flip
-    #         else transforms.Lambda(lambda x: x),
-    #         transforms.ToTensor(),
-    #         transforms.Normalize([0.5], [0.5]),
-    #     ]
-    # )
-
-    # def preprocess_train(examples):
-    #     images = [image.convert("RGB") for image in examples[image_column]]
-    #     examples["pixel_values"] = [train_transforms(image) for image in images]
-    #     examples["input_ids"] = tokenize_captions(examples)
-    #     return examples
 
     with accelerator.main_process_first():
         if args.max_train_samples is not None:

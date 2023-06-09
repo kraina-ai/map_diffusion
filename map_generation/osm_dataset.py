031bc9ffcf4953f756b4322fdce063c4ce19206a
mprzymus <przymus@gmail.com>
2023-06-09 13:09:14 +0200

    fix: remove yes from sentence, punctuation fix

--- map_generation/osm_dataset.py
+++ map_generation/osm_dataset.py
@@ -20,14 +20,18 @@ def get_columns(row, n_columns=5) -> pd.Series:
 
 def create_sentence(row: pd.Series, n_columns: int = 5) -> str:
     columns = get_columns(row, n_columns)
+    ls = [
+        _create_str_from_field(field, value) for (field, value) in row[columns].items()
+    ]
+    text_comma_end = "OSM of area containing: " + "".join(ls)
+    return text_comma_end[:-2] + "."
+
+
+def _create_str_from_field(field, value):
     space = " "
     underscore = "_"
-    ls = [
-        f"{value} {str(field).replace(underscore, space)}"
-        + ("s " if value > 1 else space)
-        for (field, value) in row[columns].items()
-    ]
-    return "OSM of area containing: " + "".join(ls) + "."
+    str_field = str(field).replace("_yes", "").replace(underscore, space)
+    return f"{value} {str_field}" + ("s " if value > 1 and len(str_field) != 0 else space) + " , "
 
 
 class TextToImageDataset(Dataset):
@@ -40,7 +44,7 @@ class TextToImageDataset(Dataset):
         random_flip=False,
         save_texts=False,
         tokenizer_path: str = BASE_MODEL_NAME,
-        cache_dir = None
+        cache_dir=None,
     ) -> None:
         super().__init__()
         self.texts = []
@@ -64,9 +68,11 @@ class TextToImageDataset(Dataset):
         self.tokenizer = CLIPTokenizer.from_pretrained(
             tokenizer_path, subfolder="tokenizer"
         )
-        self.dataset = load_dataset(path, cache_dir=cache_dir).map(
-            self.prepare_data, batched=True
-        ).with_format("pt")
+        self.dataset = (
+            load_dataset(path, cache_dir=cache_dir)
+            .map(self.prepare_data, batched=True)
+            .with_format("pt")
+        )
         if self.save_texts:
             pd.Series(self.texts).to_csv(
                 os.path.join(path, f"texts_{str(datetime.datetime.now())}.csv")

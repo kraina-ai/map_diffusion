5e037c824b9e4d7ebff4e69b3ac6d7a967946b3e
mprzymus <pzrymus@gmail.com>
2023-06-15 02:55:22 +0200

    feat: unet model

--- map_generation/masks/model.py
+++ map_generation/masks/model.py
@@ -1,20 +1,56 @@
+from typing import Callable
+
 import pytorch_lightning as pl
 import torch
-from einops import rearrange
-from pytorch_fid.fid_score import calculate_frechet_distance
 from torch import nn as nn
-from torch.nn import functional as F
 from torchmetrics import Accuracy
-from torchmetrics.image.fid import FrechetInceptionDistance
-from transformers import CLIPTextModel
-from typing import Callable
+
+
+class Unet(nn.Module):
+    def __init__(self, channels, out_labels, dropout=0.0):
+        super().__init__()
+        self.conv_layers = nn.ModuleList()
+        for in_channel, out_channel in zip([3] + channels, channels):
+            self.conv_layers.append(
+                torch.nn.Conv2d(in_channel, out_channel, 3, padding=1)
+            )
+        self.last_conv = torch.nn.Conv2d(channels[-1], channels[-1], 3, padding=1)
+
+        channels_without_last = ([out_labels] + channels)[:-1]
+        self.up_conv_layers = nn.ModuleList()
+        for out_channel, in_channel in zip(channels_without_last[::-1], channels[::-1]):
+            self.up_conv_layers.append(
+                torch.nn.ConvTranspose2d(2 * in_channel, out_channel, 2, stride=2)
+            )
+        self.pool = torch.nn.MaxPool2d(2)
+        self.act = torch.nn.ReLU()
+        self.bnorm = torch.nn.BatchNorm2d(channels[-1])
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, x):
+        shorted = []
+        for conv_layer in self.conv_layers:
+            x = self.dropout(x)
+            x = self.act(conv_layer(x))
+            x = self.pool(x)
+            shorted.append(x)
+
+        x = self.bnorm(x)
+        x = self.last_conv(x)
+
+        for up_layer, sh in zip(self.up_conv_layers, shorted[::-1]):
+            x = self.dropout(x)
+            x = up_layer(torch.cat((self.act(x), sh), dim=1))
+        return {"out": x}
 
 
 class SegmentationModel(nn.Module):
     def __init__(self, segmentation_base: nn.Module, out_conv_in=21) -> None:
         super().__init__()
         self.model = segmentation_base
-        self.out_layer = nn.Conv2d(out_conv_in, 1, 1)
+        self.out_layer = (
+            nn.Conv2d(out_conv_in, 1, 1) if out_conv_in is not None else nn.Identity()
+        )
 
     def forward(self, x):
         out = self.model(x)["out"]
